1768597650373a160f73107d66c61f43bd2a72a2
msulemansubhani92-star <m.sulemansubhani92@gmail.com>
2026-04-06 09:16:43 +0500

    Add files via upload

---
+++ src/utils/data.js
@@ -0,0 +1,26 @@
+export const navItems = [
+  { Id: '1', Link: '/articles', title: 'Article' },
+  { Id: '2', Link: '/sports', title: 'Sports' },
+  { Id: '3', Link: '/music', title: 'Music' },
+  { Id: '4', Link: '/art', title: 'Art' },
+  { Id: '5', Link: '/history', title: 'History' }
+];
+
+export const newsData = 
+ [
+  {
+    "id": 1,
+    "title": "News 1",
+    "feed": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus molestias blanditiis hic explicabo id dignissimos est autem aspernatur, itaque quis ea qui maiores voluptas eos beatae dolore quod. Temporibus, dicta?"
+  },
+  {
+    "id": 2,
+    "title": "News 2",
+    "feed": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus molestias blanditiis hic explicabo id dignissimos est autem aspernatur, itaque quis ea qui maiores voluptas eos beatae dolore quod. Temporibus, dicta?"
+  },
+  {
+    "id": 3,
+    "title": "News 3",
+    "feed": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus molestias blanditiis hic explicabo id dignissimos est autem aspernatur, itaque quis ea qui maiores voluptas eos beatae dolore quod. Temporibus, dicta?"
+  }
+]
